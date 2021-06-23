f7298ed27a566e3060d621b7b81db34155a0f618
br4vier <55928809+RafaelXavierBraga@users.noreply.github.com>
2021-06-23 16:19:37 -0300

    Commit Inicial e Resoluçao de Exercicio
    Resolução do Exercicio da Calculadora

---
+++ ProjetosEstagioInfoSis/Calculadora/ConsoleApp1/Calculadora/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+
+namespace Calculadora
+{
+    class Program
+    {
+        static void Main(String[] args)
+        {
+            Console.WriteLine("Informe uma Operação + , - , / , *");
+            string operacao = Console.ReadLine();
+            double numero1;
+            double numero2;
+
+            if (operacao == "+")
+            {
+                Console.WriteLine("Informe o valor do primeiro número:");
+                double.TryParse(Console.ReadLine() , out numero1);
+                Console.WriteLine("Informe o valor do segundo número:");
+                double.TryParse(Console.ReadLine() , out numero2);
+                Console.WriteLine(numero1 + " + " + numero2 + " = " + (numero1 + numero2));
+
+            }
+            else if (operacao == "-")
+            {
+                Console.WriteLine("Informe o valor do primeiro número:");
+                double.TryParse(Console.ReadLine() , out numero1);
+                Console.WriteLine("Informe o valor do segundo número:");
+                double.TryParse(Console.ReadLine() , out numero2);
+                Console.WriteLine(numero1 + " - " + numero2 + " = " + (numero1 - numero2));
+            }
+            else if (operacao == "/")
+            {
+                Console.WriteLine("Informe o valor do primeiro número:");
+                double.TryParse(Console.ReadLine() , out numero1);
+                Console.WriteLine("Informe o valor do segundo número:");
+                double.TryParse(Console.ReadLine() , out numero2);
+                if (numero2 != 0)
+                {
+                    Console.WriteLine(numero1 + " / " + numero2 + " = " + (numero1 / numero2));
+                }
+                else
+                {
+                    Console.WriteLine("Divisão por zero.");
+                }
+
+            }
+            else if (operacao == "*")
+            {
+                Console.WriteLine("Informe o valor do primeiro número:");
+                double.TryParse(Console.ReadLine() , out numero1);
+                Console.WriteLine("Informe o valor do segundo número:");
+                double.TryParse(Console.ReadLine() , out numero2);
+                Console.WriteLine(numero1 + " * " + numero2 + " = " + (numero1 * numero2));
+            }
+            else
+            {
+                Console.WriteLine("Operação Invalida");
+            }
+        }
+    }
+}
