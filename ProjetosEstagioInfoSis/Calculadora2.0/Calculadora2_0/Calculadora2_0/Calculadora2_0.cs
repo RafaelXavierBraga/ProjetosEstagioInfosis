b9b57ff395d884632fb80780cb282f77585c1d68
br4vier <rafaelxavierbraga@gmail.com>
2021-06-24 12:52:05 -0300

    Commit Calculadora2_0

---
+++ ProjetosEstagioInfoSis/Calculadora2.0/Calculadora2_0/Calculadora2_0/Calculadora2_0.cs
@@ -0,0 +1,90 @@
+﻿using System;
+
+namespace Calculadora2_0
+{
+    class Calculadora2_0
+    {
+        public static string VerificaReuso(string operacao) {
+         
+            Console.WriteLine("Deseja fazer uma nova operação? 'sim' ou 'sair'");
+            operacao = Console.ReadLine();
+            return operacao;
+        }
+
+        static void Main(string[] args)
+        { 
+            double numero1;
+            double numero2;
+            string operacao;
+            
+            do
+            {
+                Console.WriteLine("Informe uma Operação (+ , - , / , *)");
+                Console.WriteLine("Caso queira sair digite: 'sair'.");
+                operacao = Console.ReadLine();
+                if (operacao == "+")
+                {
+                    Console.WriteLine("Informe o valor do primeiro número:");
+                    double.TryParse(Console.ReadLine(), out numero1);
+                    Console.WriteLine("Informe o valor do segundo número:");
+                    double.TryParse(Console.ReadLine(), out numero2);
+                    Console.WriteLine(numero1 + " + " + numero2 + " = " + (numero1 + numero2));
+
+                    operacao = VerificaReuso(operacao);
+
+                }
+                else if (operacao == "-")
+                {
+                    Console.WriteLine("Informe o valor do primeiro número:");
+                    double.TryParse(Console.ReadLine(), out numero1);
+                    Console.WriteLine("Informe o valor do segundo número:");
+                    double.TryParse(Console.ReadLine(), out numero2);
+                    Console.WriteLine(numero1 + " - " + numero2 + " = " + (numero1 - numero2));
+
+                    operacao = VerificaReuso(operacao);
+                }
+                else if (operacao == "/")
+                {
+                    Console.WriteLine("Informe o valor do primeiro número:");
+                    double.TryParse(Console.ReadLine(), out numero1);
+                    Console.WriteLine("Informe o valor do segundo número:");
+                    double.TryParse(Console.ReadLine(), out numero2);
+                    if (numero2 != 0)
+                    {
+                        Console.WriteLine(numero1 + " / " + numero2 + " = " + (numero1 / numero2));
+                    }
+                    else
+                    {
+                        Console.WriteLine("Divisão por zero.");
+                    }
+                    operacao = VerificaReuso(operacao);
+                }
+                else if (operacao == "*")
+                {
+                    Console.WriteLine("Informe o valor do primeiro número:");
+                    double.TryParse(Console.ReadLine(), out numero1);
+                    Console.WriteLine("Informe o valor do segundo número:");
+                    double.TryParse(Console.ReadLine(), out numero2);
+                    Console.WriteLine(numero1 + " * " + numero2 + " = " + (numero1 * numero2));
+
+                    operacao = VerificaReuso(operacao);
+                }
+                else
+                {
+                    if (operacao != "sair")
+                    {
+                        Console.Clear();
+                        Console.WriteLine("Operação Invalida");  
+                    }
+                    
+                }
+                if (operacao == "sim")
+                {
+                    Console.Clear();
+                    Console.WriteLine("Nova Operação:");
+                }
+            } while (operacao != "sair");
+            
+        }
+    }
+}
