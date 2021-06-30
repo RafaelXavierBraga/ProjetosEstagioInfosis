46494624dc9276bc0c7c72d11cceb5640d389921
br4vier <55928809+RafaelXavierBraga@users.noreply.github.com>
2021-06-30 13:25:12 -0300

    Commit com configurações

---
+++ ProjetosEstagioInfoSis/VilaoEletrico/VilaoEletrico/VilaoEletrico/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace VilaoEletrico
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Sistema sistema = new Sistema();
+            
+            sistema.CadastrarValoresDoSistema();
+            sistema.CadastrarItens();
+            sistema.ImprimeRelatorio();
+        }
+    }
+}
